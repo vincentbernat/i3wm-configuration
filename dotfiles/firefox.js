8cb90e03da6d92e609e639efc7b9c4163984162e
Vincent Bernat <vincent@bernat.ch>
2021-07-14 18:16:58 +0200

    firefox: don't resist fingerprinting
    So many stuff are breaking, including switching tabs use Alt+N. Switch
    back to systemUsesDarkTheme 0 for the moment...

--- dotfiles/firefox.js
+++ dotfiles/firefox.js
@@ -4,7 +4,7 @@
 // Theme
 user_pref("extensions.activeThemeID", "default-theme@mozilla.org");
 user_pref("devtools.theme", "dark");
-user_pref("privacy.resistFingerprinting", true); // prefers-color-scheme: light
+user_pref("ui.systemUsesDarkTheme", 0);
 
 // Ensure context menus stay open after left-click (useful when scale
 // == 1.5)

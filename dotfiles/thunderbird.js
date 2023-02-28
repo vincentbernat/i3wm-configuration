fc713c43e12d0763b729e6547e0fabb5d0524d47
Vincent Bernat <vincent@bernat.ch>
2023-02-28 10:13:55 +0100

    thunderbird: add user.js

---
+++ dotfiles/thunderbird.js
@@ -0,0 +1,59 @@
+// Preferences for Thunderbird. To be symlinked in the profile as user.js.
+
+// Theme
+user_pref("extensions.activeThemeID", "default-theme@mozilla.org");
+user_pref("layout.css.prefers-color-scheme.content-override", 1); // light
+user_pref("mail.uidensity", 1);
+
+// Fonts
+user_pref("font.name.monospace.x-western", "Source Code Pro");
+user_pref("font.name.monospace.x-unicode", "Source Code Pro");
+user_pref("font.name.sans-serif.x-western", "Source Sans Pro");
+user_pref("font.name.sans-serif.x-unicode", "Source Sans Pro");
+user_pref("font.name.serif.x-western", "Source Serif Pro");
+user_pref("font.name.serif.x-unicode", "Source Serif Pro");
+user_pref("browser.display.use_document_fonts", false);
+user_pref("font.minimum-size.x-western", 14);
+user_pref("font.minimum-size.x-unicode", 14);
+user_pref("font.size.monospace.x-western", 14);
+user_pref("font.size.monospace.x-unicode", 14);
+user_pref("font.size.variable.x-western", 14);
+user_pref("font.size.variable.x-unicode", 14);
+
+// Ensure context menus stay open after left-click (useful when scale
+// == 1.5)
+user_pref("ui.context_menus.after_mouseup", true);
+
+// Don't display menubar when pressing Alt
+user_pref("ui.key.menuAccessKeyFocuses", false);
+// Keep GTK keybindings
+user_pref("ui.key.use_select_all_in_single_line_editor", false);
+
+// Calendar
+user_pref("calendar.alarms.playsound", false);
+user_pref("calendar.alarms.show", false);
+user_pref("calendar.alarms.showmissed", false);
+user_pref("calendar.week.start", 1);
+
+// Dates
+user_pref("intl.date_time.pattern_override.date_short", "yyyy-MM-dd");
+user_pref("intl.date_time.pattern_override.time_short", "HH:mm");
+
+// Mail
+user_pref("mail.biff.alert.show_preview", false);
+user_pref("mail.biff.alert.play_sound", false);
+user_pref("mail.biff.alert.show_alert", false);
+user_pref("mail.collect_email_address_outgoing", false);
+user_pref("mail.compose.catchAllHeaders", "envelope-to, x-original-to, to, cc, x-delivered-to");
+user_pref("mail.openpgp.allow_external_gnupg", true);
+user_pref("mail.operate_on_msgs_in_collapsed_threads", false);
+user_pref("mail.phishing.detection.enabled", false);
+user_pref("mail.purge.ask", false);
+user_pref("mail.server.default.check_all_folders_for_new", true);
+user_pref("mail.warn_on_send_accel_key", false);
+user_pref("mailnews.headers.useMinimalUserAgent", true);
+user_pref("mailnews.headers.showUserAgent", true);
+
+// Offline
+user_pref("offline.download.download_messages", 1);
+user_pref("offline.send.unsent_messages", 1);

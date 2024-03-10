6a1da63238688e94e08cb1066c3ed0a03983119b
Vincent Bernat <vincent@bernat.ch>
2024-03-10 21:02:40 +0100

    thunderbird: don't use default colors for message compose

--- dotfiles/thunderbird.js
+++ dotfiles/thunderbird.js
@@ -4,6 +4,7 @@
 user_pref("extensions.activeThemeID", "thunderbird-compact-dark@mozilla.org");
 user_pref("layout.css.prefers-color-scheme.content-override", 1); // light
 user_pref("layout.css.color-scheme.enabled", false);
+user_pref("msgcompose.default_colors", false);
 user_pref("mail.uidensity", 1);
 
 // Fonts

4d0944b638420b53cd2a3d50b510cb5e9772f2f7
Vincent Bernat <vincent@bernat.ch>
2024-07-14 07:30:41 +0200

    firefox: don't suggest recent searches in url bar

--- dotfiles/firefox.js
+++ dotfiles/firefox.js
@@ -98,6 +98,9 @@ user_pref("browser.safebrowsing.malware.enabled", false);
 // Don't trim URLs
 user_pref("browser.urlbar.trimURLs", false);
 
+// Don't show recent searchs
+user_pref("browser.urlbar.suggest.recentsearches", false);
+
 // Don't offer translating
 user_pref("browser.translations.automaticallyPopup", false);
 user_pref("browser.translations.panelShown", true);
